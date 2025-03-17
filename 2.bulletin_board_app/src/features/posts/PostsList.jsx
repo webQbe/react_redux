336741a30f3d70b0213cf98e48c0a3d57185034c
webQbe <webcubesolutionslk@gmail.com>
2025-03-17 20:12:59 +0530

    Display author’s name for each post

--- 2.bulletin_board_app/src/features/posts/PostsList.jsx
+++ 2.bulletin_board_app/src/features/posts/PostsList.jsx
@@ -1,7 +1,7 @@
 /* Retrieve and display posts from Redux*/
 import { useSelector } from 'react-redux'; // Used to fetch posts
 import { selectAllPosts } from './postSlice'; // Retrieve all posts from the Redux store
-
+import PostAuthor from './PostAuthor'; // Render Posts with Author Info
 
 const PostsList = () => {
     // Retrieve Posts from Redux Store
@@ -9,10 +9,16 @@ const PostsList = () => {
 
     // Map over the list of posts and creates <article> elements for each post
     // Use substring(0, 100) to limit content to 100 characters
+    // Include <PostAuthor /> component for each post, pass post.userId as a prop
     const renderedPosts = posts.map(post => (
         <article key={post.id}>
             <h3>{post.title}</h3>
             <p>{post.content.substring(0, 100)}</p>
+            <p className="postCredit">
+                <PostAuthor 
+                    userId={post.userId} /* display the correct author for each post */
+                />
+            </p>
         </article>
     ))
 
