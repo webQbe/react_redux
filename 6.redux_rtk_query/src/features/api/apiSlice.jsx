2065204b81c36bb831840549da2e4f93033f7e68
webQbe <webcubesolutionslk@gmail.com>
2025-03-25 19:36:09 +0530

    Enhanced apiSlice config by adding tag-based caching and invalidation

--- 6.redux_rtk_query/src/features/api/apiSlice.jsx
+++ 6.redux_rtk_query/src/features/api/apiSlice.jsx
@@ -8,31 +8,40 @@ export const apiSlice = createApi({
     baseQuery: fetchBaseQuery({ // Configure fetchBaseQuery
             baseUrl: 'http://localhost:3500' // Fetch mock data from local JSON server
         }),
+    tagTypes: ['Todos'], // Register 'Todos' tag to track changes in the todo list.
+
     /* Define mutation endpoints */
     endpoints: (builders) => ({
         getTodos: builders.query({ // Query that fetches todos from the /todos endpoint
             query: () => '/todos',
+            providesTags: ['Todos'] // Provide data under the 'Todos' tag
+            /* This tells RTK Query that this tag represents the list of todos */
         }),
         addTodo: builders.mutation({ // Send POST request to /todos with a new todo item
             query: (todo) => ({
                 url: '/todos',
                 method: 'POST',
                 body: todo
-            })
+            }),
+            // Each mutation (add, update, delete) invalidates the 'Todos' tag
+            invalidatesTags: ['Todos']
+            /* After any of these mutations occur, RTK Query automatically refetches the getTodos query, to ensure the UI always displays the latest todo list without manually triggering a refetch. */
         }),
         updateTodo: builders.mutation({ // Send PATCH request to update a specific todo by id
             query: (todo) => ({
                 url: `/todos/${todo.id}`,
                 method: 'PATCH',
                 body: todo
-            })
+            }),
+            invalidatesTags: ['Todos']
         }),
         deleteTodo: builders.mutation({ // Send DELETE request to remove a todo by id
             query: ({ id }) => ({
                 url: `/todos/${id}`,
                 method: 'DELETE',
                 body: id
-            })
+            }),
+            invalidatesTags: ['Todos']
         }),
     })
 })
